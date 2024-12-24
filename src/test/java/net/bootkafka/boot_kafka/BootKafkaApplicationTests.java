672fb963cc2685e3f5cb4e1236d3d25048734a92
Rishabh-mac <raj.rishabh01@gmail.com>
2024-12-24 17:04:39 -0500

    Initial commit

---
+++ src/test/java/net/bootkafka/boot_kafka/BootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bootkafka.boot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
