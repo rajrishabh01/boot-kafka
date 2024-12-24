eb93aa68ec8b51237dc7b54a8010012040cc13ef
Rishabh-mac <raj.rishabh01@gmail.com>
2024-12-24 17:07:19 -0500

    Initial commit

--- src/main/java/net/bootkafka/boot_kafka/BootKafkaApplication.java
+++ src/main/java/net/bootkafka/boot_kafka/BootKafkaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootKafkaApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(BootKafkaApplication.class, args);
 	}
