29a856e9b64a9152f072b57bdfe4040301a04fa3
Rishabh-mac <raj.rishabh01@gmail.com>
2024-12-28 16:33:02 -0500

    kafka consumer

---
+++ src/main/java/net/bootkafka/boot_kafka/kafka/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package net.bootkafka.boot_kafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "firstTop", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message received -> %s", message));
+
+    }
+}
